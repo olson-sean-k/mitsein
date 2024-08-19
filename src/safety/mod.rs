941c53b98d7546b88b85586ef60c9e0d53452dca
Sean Olson <olson.sean.k@gmail.com>
2024-08-19 12:00:39 -0700

    Allow `dead_code` and `unused_imports` in the `safety` module.

--- src/safety/mod.rs
+++ src/safety/mod.rs
@@ -1,9 +1,13 @@
+// Avoid `cfg` attributes in these APIs. This requires more careful auditing, since the linter
+// cannot warn when an API is dead.
+#![allow(dead_code)]
+
 // Checked implementation of extension traits. Failures panic.
 #[cfg(all(not(miri), test))]
 #[path = "checked.rs"]
 mod maybe;
-// Unchecked implementation of extension traits. Failures are ignored or unobserved, which may be
-// UB.
+// Unchecked implementation of extension traits. Failures are ignored or unobserved. This is UB if
+// the crate implementation is incorrect or its APIs are unsound!
 #[cfg(not(all(not(miri), test)))]
 #[path = "unchecked.rs"]
 mod maybe;
@@ -12,7 +16,7 @@ use core::slice::SliceIndex;
 
 // TODO: At time of writing, traits cannot expose `const` functions. Remove this in favor of
 //       `NonZeroExt` when this is possible. See `array_vec1`.
-#[cfg(feature = "arrayvec")]
+#[allow(unused_imports)]
 pub use maybe::non_zero_from_usize_maybe_unchecked;
 
 pub trait NonZeroExt<T> {
